8df2db1e14b9a5be2fb15cf2cd820e13d23aced4
fernandonito <jfernando.nito@gmail.com>
2023-03-30 21:21:58 -0300

    Next Head Component

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,6 +1,13 @@
+import Head from 'next/head';
+
 function HomePage() {
+  console.log('[HomePage] render');
   return (
     <>
+    <Head>
+      <title>My blog</title>
+      <meta name="description" value="This is my blog" />
+    </Head>
       <main>
         <h1>My blog</h1>
       </main>
