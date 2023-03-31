0a36541e57dfcc9940950b7fff3852dbf3d41793
fernandonito <jfernando.nito@gmail.com>
2023-03-30 21:16:40 -0300

    Custom App Component

--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,12 +1,6 @@
-import NavBar from '../components/NavBar';
-
 function HomePage() {
-  console.log('[HomePage] render');
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>My blog</h1>
       </main>
