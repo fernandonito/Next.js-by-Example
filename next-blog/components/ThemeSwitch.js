8e1af657f7db7a8b78e5b1a509612d80d3df86a1
fernandonito <jfernando.nito@gmail.com>
2023-04-07 16:29:52 -0300

    Feature Detection

--- next-blog/components/ThemeSwitch.js
+++ next-blog/components/ThemeSwitch.js
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DarkTheme from "./DarkTheme";
 
 function ThemeSwitch() {
+  useEffect(() => {
+    const value = localStorage.getItem('darkMode');
+    const mode = value && JSON.parse(value);
+    setDarkMode(mode);
+  }, []);
+
   const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = () => {
+    localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
 
   console.log('[ThemeSwitch] render', darkMode);
   const text = darkMode ? 'Light Mode' : 'Dark Mode';
 
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>{ text }</button>
+      <button onClick={handleClick}>{ text }</button>
       <style jsx>{`
         button {
           background: none;
