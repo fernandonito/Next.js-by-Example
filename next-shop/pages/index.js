4971cb52cfc735bef962914aa82b54638f0918d5
fernandonito <jfernando.nito@gmail.com>
2023-08-06 16:06:44 -0300

    Create Next App

---
+++ next-shop/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main>
+        <h1>Next Shop</h1>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
