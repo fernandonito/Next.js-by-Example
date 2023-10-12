12d02d7653f5f9e0fe7b3078b6ed04e14e21a496
Fernando Nito <jfernando.nito@gmail.com>
2023-10-12 19:01:27 -0300

    Product Page

--- next-shop/lib/products.js
+++ next-shop/lib/products.js
@@ -1,10 +1,16 @@
 function stripProduct(product) {
-  const { id, title } = product;
-  return { id, title };
+  const { id, title, description } = product
+  return { id, title, description }
+}
+
+export async function getProduct(id) {
+  const response = await fetch(`http://localhost:1337/products/${id}`)
+  const product = await response.json()
+  return stripProduct(product)
 }
 
 export async function getProducts() {
-  const response = await fetch('http://localhost:1337/products');
-  const products = await response.json();
-  return products.map(stripProduct);
+  const response = await fetch('http://localhost:1337/products')
+  const products = await response.json()
+  return products.map(stripProduct)
 }
