59cdcd1289b298b0d92b3339eb983e75f9abf3b0
Fernando Nito <jfernando.nito@gmail.com>
2023-10-13 21:22:59 -0300

    Responsive Product Page

--- next-shop/pages/products/[id].js
+++ next-shop/pages/products/[id].js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Title from '@/components/Title'
 import { getProduct, getProducts } from '@/lib/products'
 import { ApiError } from '@/lib/api'
@@ -39,7 +40,17 @@ function ProductPage({ product }) {
       </Head>
       <main className="px-6 py-4">
         <Title> {product.title} </Title>
-        <p> {product.description} </p>
+        <div className="flex flex-col lg:flex-row">
+          <div>
+            <Image src={product.pictureUrl} alt="" width={640} height={480} />
+          </div>
+          <div className="flex-1 lg:ml-4">
+            <p className="text-sm">{product.description}</p>
+            <p className="text-lg font-bold mt-2">{`$${product.price.toFixed(
+              2
+            )}`}</p>
+          </div>
+        </div>
       </main>
     </>
   )
