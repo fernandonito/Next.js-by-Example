0dd0f714be5c7b4d74dc50cced301dae331fdfec
Fernando Nito <jfernando.nito@gmail.com>
2023-10-15 19:57:47 -0300

    Login API Route

--- next-shop/pages/api/login.js
+++ next-shop/pages/api/login.js
@@ -1,9 +1,24 @@
+import { fetchJson } from '@/lib/api'
+
 async function handleLogin(req, res) {
   if (req.method !== 'POST') {
     res.status(405).end()
     return
   }
-  res.status(200).json({})
+  const { email, password } = req.body
+  try {
+    const { jwt, user } = await fetchJson('http://localhost:1337/auth/local', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ identifier: email, password }),
+    })
+    res.status(200).json({
+      id: user.id,
+      name: user.username,
+    })
+  } catch (err) {
+    res.status(401).end()
+  }
 }
 
 export default handleLogin
