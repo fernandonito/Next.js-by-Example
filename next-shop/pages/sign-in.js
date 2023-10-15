0b66d0478b3145bfaf2fe0d931b36581af88b992
Fernando Nito <jfernando.nito@gmail.com>
2023-10-15 18:10:29 -0300

    Sign In Form

--- next-shop/pages/sign-in.js
+++ next-shop/pages/sign-in.js
@@ -1,9 +1,20 @@
 import Page from '@/components/Page'
+import Field from '@/components/Field'
+import Input from '@/components/Input'
+import Button from '@/components/Button'
 
 function SignInPage() {
   return (
     <Page title="Sign In">
-      <p>Sign In</p>
+      <form>
+        <Field label="Email">
+          <Input type="email" />
+        </Field>
+        <Field label="Password">
+          <Input type="password" />
+        </Field>
+        <Button type="submit">Sign In</Button>
+      </form>
     </Page>
   )
 }
