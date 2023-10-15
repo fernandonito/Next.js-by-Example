85a8cce346ee33aa367ce26d7e316219737378cd
Fernando Nito <jfernando.nito@gmail.com>
2023-10-15 19:00:06 -0300

    Form State and Validation

--- next-shop/pages/sign-in.js
+++ next-shop/pages/sign-in.js
@@ -1,17 +1,36 @@
+import { useState } from 'react'
 import Page from '@/components/Page'
 import Field from '@/components/Field'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
 
 function SignInPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(`Email: ${email}, Password: ${password}`)
+  }
+
   return (
     <Page title="Sign In">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field label="Email">
-          <Input type="email" />
+          <Input
+            type="email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Field>
         <Field label="Password">
-          <Input type="password" />
+          <Input
+            type="password"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Field>
         <Button type="submit">Sign In</Button>
       </form>
