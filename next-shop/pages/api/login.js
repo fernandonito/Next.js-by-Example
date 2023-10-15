11f0c610f27f6c0b53cd8ba69d26519abd76a6d4
Fernando Nito <jfernando.nito@gmail.com>
2023-10-15 20:03:31 -0300

    Setting a Cookie

--- next-shop/pages/api/login.js
+++ next-shop/pages/api/login.js
@@ -1,4 +1,6 @@
+import cookie from 'cookie'
 import { fetchJson } from '@/lib/api'
+const { CMS_URL } = process.env
 
 async function handleLogin(req, res) {
   if (req.method !== 'POST') {
@@ -7,15 +9,24 @@ async function handleLogin(req, res) {
   }
   const { email, password } = req.body
   try {
-    const { jwt, user } = await fetchJson('http://localhost:1337/auth/local', {
+    const { jwt, user } = await fetchJson(`${CMS_URL}/auth/local`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ identifier: email, password }),
     })
-    res.status(200).json({
-      id: user.id,
-      name: user.username,
-    })
+    res
+      .status(200)
+      .setHeader(
+        'Set-Cookie',
+        cookie.serialize('token', jwt, {
+          path: '/api',
+          httpOnly: true,
+        })
+      )
+      .json({
+        id: user.id,
+        name: user.username,
+      })
   } catch (err) {
     res.status(401).end()
   }
