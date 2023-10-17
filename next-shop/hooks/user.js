bb6b9390ccb0f9ae933863f2241a690f933c03b9
Fernando Nito <jfernando.nito@gmail.com>
2023-10-16 21:35:11 -0300

    useSignIn Custom Hook

--- next-shop/hooks/user.js
+++ next-shop/hooks/user.js
@@ -1,5 +1,29 @@
-import { useQuery } from 'react-query'
+import { useQueryClient, useMutation, useQuery } from 'react-query'
 import { fetchJson } from '@/lib/api'
+
+export function useSignIn() {
+  const queryClient = useQueryClient()
+  const mutation = useMutation(({ email, password }) =>
+    fetchJson('/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    })
+  )
+  return {
+    signIn: async (email, password) => {
+      try {
+        await mutation.mutateAsync({ email, password })
+        queryClient.setQueryData('user', user)
+        return true
+      } catch (err) {
+        return false
+      }
+    },
+    signInError: mutation.isError,
+    signInLoading: mutation.isLoading,
+  }
+}
 
 export function useUser() {
   const query = useQuery(
--- next-shop/pages/sign-in.js
+++ next-shop/pages/sign-in.js
@@ -1,33 +1,21 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import { useSignIn } from '@/hooks/user'
 import Page from '@/components/Page'
 import Field from '@/components/Field'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
-import { fetchJson } from '@/lib/api'
-import { useMutation } from 'react-query'
 
 function SignInPage() {
   const router = useRouter()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const mutation = useMutation(() =>
-    fetchJson('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
-    })
-  )
+  const { signIn, signInError, signInLoading } = useSignIn()
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    try {
-      const user = await mutation.mutateAsync()
-      console.log('signed in:', user)
-      router.push('/')
-    } catch (err) {
-      //mutation.isError will be true
-    }
+    const valid = signIn(email, password)
+    if (valid) router.push('/')
   }
 
   return (
@@ -49,10 +37,10 @@ function SignInPage() {
             onChange={(event) => setPassword(event.target.value)}
           />
         </Field>
-        {mutation.isError && (
+        {signInError && (
           <p className="text-red-700">Invalid credentials. Please try again.</p>
         )}
-        {mutation.isLoading ? (
+        {signInLoading ? (
           <p>Loading...</p>
         ) : (
           <Button type="submit">Sign In</Button>
