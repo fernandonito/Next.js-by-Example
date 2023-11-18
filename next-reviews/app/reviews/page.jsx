743aab28c78a40bf3bd0e4e951955e1df157f4a9
Fernando Nito <jfernando.nito@gmail.com>
2023-11-17 21:20:56 -0300

    Rendering Arrays

--- next-reviews/app/reviews/page.jsx
+++ next-reviews/app/reviews/page.jsx
@@ -4,40 +4,30 @@ import { getReviews } from '@/lib/reviews'
 
 export default async function ReviewsPage() {
   const reviews = await getReviews()
-  console.log('[ReviewsPage] rendering', reviews)
 
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img
-              src="/images/hollow-knight.jpg"
-              alt="Hollow Knight"
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li
+            key={review.slug}
+            className="bg-white border rounded shadow w-80 hover:shadow-xl"
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt={review.title}
+                width="320"
+                height="180"
+                className="rounded-t"
+              />
+            </Link>
             <h2 className="font-semibold font-orbitron py-1 text-center">
-              Hollow Knight
+              {review.title}
             </h2>
-          </Link>
-        </li>
-        <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img
-              src="/images/stardew-valley.jpg"
-              alt="Stardew Valley"
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="font-semibold font-orbitron py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+          </li>
+        ))}
       </ul>
     </>
   )
