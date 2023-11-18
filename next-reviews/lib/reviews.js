c7d087949c6b7c7490175d8f5a6f7fd6bf503003
Fernando Nito <jfernando.nito@gmail.com>
2023-11-17 21:02:34 -0300

    Listing Files

--- next-reviews/lib/reviews.js
+++ next-reviews/lib/reviews.js
@@ -1,4 +1,4 @@
-import { readFile } from 'node:fs/promises'
+import { readdir, readFile } from 'node:fs/promises'
 import matter from 'gray-matter'
 import { marked } from 'marked'
 
@@ -10,5 +10,13 @@ export async function getReview(slug) {
   } = matter(text)
   const body = marked(content)
 
-  return { title, date, image, body }
+  return { slug, title, date, image, body }
+}
+
+export async function getReviews() {
+  const files = await readdir('./content/reviews')
+  const slugs = files.map((filename) => filename.replace('.md', ''))
+
+  const reviews = await Promise.all(slugs.map(getReview))
+  return reviews
 }
