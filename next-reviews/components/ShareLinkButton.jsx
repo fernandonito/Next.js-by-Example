f8f54a7cfceca56aede313215b2f486d34510164
Fernando Nito <jfernando.nito@gmail.com>
2023-11-22 21:20:02 -0300

    Share Link Button

---
+++ next-reviews/components/ShareLinkButton.jsx
@@ -0,0 +1,14 @@
+export default function ShareLinkButton() {
+  const handleClick = () => {
+    console.log('Share link')
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
+    >
+      Share link
+    </button>
+  )
+}
