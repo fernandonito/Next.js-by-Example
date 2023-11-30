7b0b177488ba5aac6aa51613d3a779d8988547bc
Fernando Nito <jfernando.nito@gmail.com>
2023-11-29 21:42:12 -0300

    Fetch API

--- next-reviews/scripts/strapi-request.mjs
+++ next-reviews/scripts/strapi-request.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'node:fs'
 
-const url = 'http://localhost:1337/api/reviews'
+const url = 'http://localhost:1337/api/reviews' + '?populate=*'
 const response = await fetch(url)
 const body = await response.json()
 const formatted = JSON.stringify(body, null, 2)
