1fbbd2106005ae078e6a826e7daff65dacf87833
Fernando Nito <jfernando.nito@gmail.com>
2023-11-30 21:47:00 -0300

    Get Review From CMS - Step 1

--- next-reviews/scripts/strapi-request.mjs
+++ next-reviews/scripts/strapi-request.mjs
@@ -2,14 +2,15 @@ import { writeFileSync } from 'node:fs'
 import qs from 'qs'
 
 const url =
-  'http://localhost:1337/api/reviews' +
+  'http://localhost:1337/api/reviews/8' +
   '?' +
   qs.stringify(
     {
-      fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+      filters: { slug: { $eq: 'hades-2018' } },
+      fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
       populate: { image: { fields: ['url'] } },
-      sort: ['publishedAt:desc'],
-      pagination: { pageSize: 6 },
+      // sort: ['publishedAt:desc'],
+      pagination: { pageSize: 1, withCount: false },
     },
     { encodeValuesOnly: true }
   )
