09c6b882eda6f944e3ff8c5d9ecfae352b49916f
Fernando Nito <jfernando.nito@gmail.com>
2023-11-30 22:08:09 -0300

    Refactoring: Fetch Reviews

--- next-reviews/lib/reviews.js
+++ next-reviews/lib/reviews.js
@@ -11,60 +11,55 @@ export async function getFeaturedReview() {
 }
 
 export async function getReview(slug) {
-  const url =
-    `${CMS_URL}/api/reviews?` +
-    qs.stringify(
-      {
-        filters: { slug: { $eq: slug } },
-        fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
-        populate: { image: { fields: ['url'] } },
-        pagination: { pageSize: 1, withCount: false },
-      },
-      { encodeValuesOnly: true }
-    )
-  console.log('getReview: ', url)
-
-  const response = await fetch(url)
-  const { data } = await response.json()
-  const {
-    attributes: { title, publishedAt, image, body },
-  } = data[0]
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+    populate: { image: { fields: ['url'] } },
+    pagination: { pageSize: 1, withCount: false },
+  })
+  const item = data[0]
 
   return {
-    slug,
-    title,
-    data: publishedAt.slice(0, 'yyyy-mm-dd'.length),
-    image: CMS_URL + image.data.attributes.url,
-    body: marked(body),
+    ...toReview(item),
+    body: marked(item.attributes.body),
   }
 }
 
 export async function getReviews() {
-  const url =
-    `${CMS_URL}/api/reviews?` +
-    qs.stringify(
-      {
-        fields: ['slug', 'title', 'subtitle', 'publishedAt'],
-        populate: { image: { fields: ['url'] } },
-        sort: ['publishedAt:desc'],
-        pagination: { pageSize: 6 },
-      },
-      { encodeValuesOnly: true }
-    )
-  console.log('getReviews: ', url)
+  const { data } = await fetchReviews({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    populate: { image: { fields: ['url'] } },
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 6 },
+  })
 
-  const response = await fetch(url)
-  const { data } = await response.json()
-
-  return data.map(({ attributes: { slug, title, publishedAt, image } }) => ({
-    slug,
-    title,
-    data: publishedAt.slice(0, 'yyyy-mm-dd'.length),
-    image: CMS_URL + image.data.attributes.url,
-  }))
+  return data.map(toReview)
 }
 
 export async function getSlugs() {
   const files = await readdir('./content/reviews')
   return files.map((filename) => filename.replace('.md', ''))
+}
+
+async function fetchReviews(parameters) {
+  const url =
+    `${CMS_URL}/api/reviews?` +
+    qs.stringify(parameters, { encodeValuesOnly: true })
+  console.log('[fetchReviews]: ', url)
+
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`)
+  }
+
+  return await response.json()
+}
+
+function toReview({ attributes: { slug, title, publishedAt, image } }) {
+  return {
+    slug,
+    title,
+    data: publishedAt.slice(0, 'yyyy-mm-dd'.length),
+    image: CMS_URL + image.data.attributes.url,
+  }
 }
