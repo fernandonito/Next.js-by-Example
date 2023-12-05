5017d29998b6b5c35bdb7598566935655e45c78f
Fernando Nito <jfernando.nito@gmail.com>
2023-12-04 22:06:05 -0300

    Exercise: Home Images

--- next-reviews/app/page.jsx
+++ next-reviews/app/page.jsx
@@ -1,31 +1,40 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import Heading from '@/components/Heading'
-import { getFeaturedReview } from '@/lib/reviews'
+import { getReviews } from '@/lib/reviews'
 
 export default async function HomePage() {
-  const review = await getFeaturedReview()
+  const reviews = await getReviews(3)
   // console.log('[HomePage] rendering')
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="pb-3">Only the best indie games, reviewed for you</p>
-      <div className="bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full">
-        <Link
-          href={`/reviews/${review.slug}`}
-          className="flex flex-col sm:flex-row"
-        >
-          <img
-            src={review.image}
-            alt={review.title}
-            width="320"
-            height="180"
-            className="rounded-t sm:rounded-l sm:rounded-r-none"
-          />
-          <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
-            {review.title}
-          </h2>
-        </Link>
-      </div>
+      <ul className="flex flex-col gap-3">
+        {reviews.map((review, index) => (
+          <li
+            key={review.slug}
+            className="bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full"
+          >
+            <Link
+              href={`/reviews/${review.slug}`}
+              className="flex flex-col sm:flex-row"
+            >
+              <Image
+                src={review.image}
+                alt={review.title}
+                priority={index < 1}
+                width="320"
+                height="180"
+                className="rounded-t sm:rounded-l sm:rounded-r-none"
+              />
+              <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
