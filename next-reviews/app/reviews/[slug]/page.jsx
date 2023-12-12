de199b318aabed1ff1c3ca86661e73744eee6e93
Fernando Nito <jfernando.nito@gmail.com>
2023-12-11 21:36:28 -0300

    Page Not Found

--- next-reviews/app/reviews/[slug]/page.jsx
+++ next-reviews/app/reviews/[slug]/page.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 import Heading from '@/components/Heading'
 import ShareButtons from '@/components/ShareButtons'
 import { getReview, getSlugs } from '@/lib/reviews'
@@ -13,6 +14,7 @@ export const dynamic = 'force-dynamic'
 
 export async function generateMetadata({ params: { slug } }) {
   const review = await getReview(slug)
+  if (!review) return notFound()
   return {
     title: review.title,
   }
@@ -21,6 +23,7 @@ export async function generateMetadata({ params: { slug } }) {
 export default async function ReviewPage({ params: { slug } }) {
   console.log('[ReviewPage] rendering', slug)
   const review = await getReview(slug)
+  if (!review) return notFound()
   return (
     <>
       <Heading>{review.title}</Heading>
