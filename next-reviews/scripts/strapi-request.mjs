0513eb530d19b4738cf997c20d6b27c343c92036
Fernando Nito <jfernando.nito@gmail.com>
2023-12-20 21:14:43 -0300

    Page Count

--- next-reviews/scripts/strapi-request.mjs
+++ next-reviews/scripts/strapi-request.mjs
@@ -2,15 +2,14 @@ import { writeFileSync } from 'node:fs'
 import qs from 'qs'
 
 const url =
-  'http://localhost:1337/api/reviews/8' +
+  'http://localhost:1337/api/reviews/' +
   '?' +
   qs.stringify(
     {
-      filters: { slug: { $eq: 'hades-2018' } },
-      fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+      fields: ['slug', 'title', 'subtitle', 'publishedAt'],
       populate: { image: { fields: ['url'] } },
-      // sort: ['publishedAt:desc'],
-      pagination: { pageSize: 1, withCount: false },
+      sort: ['publishedAt:desc'],
+      pagination: { pageSize: 6, page: 1 },
     },
     { encodeValuesOnly: true }
   )
